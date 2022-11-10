5d69dccbbb49e0fde57e7cec7935bf7022097aae
Bhimashankar03 <bhimashankarp005@gmail.com>
2022-11-10 13:20:36 +0530

    Add project files.

---
+++ LoginMVCProgram/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using LoginMVCProgram.Models; 
+
+namespace LoginMVCProgram.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+        //[HttpGet]
+        //public IActionResult Index()
+        //{
+
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult Index(string name,string password)
+        //{
+        //    if(name=="ABC" && password=="XYZ")
+        //    {
+        //        return RedirectToAction("Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+        //public IActionResult Dashboard()
+        //{
+        //    return View();
+        //}
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Student s = new Student();
+           
+             return View(s);
+
+        }
+        [HttpPost]
+        public IActionResult Index(Student s )
+        {
+            if(s.name=="ABC" && s.password=="XYZ")
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+          
+            return View();
+
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginMVCProgram/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginMVCProgram.Models
+{
+    public class Student
+    {
+        public string ClassName { get; set; } ="Bright Infotech";
+        public string name { get; set; }
+        public string password { get; set; }
+    }
+}
